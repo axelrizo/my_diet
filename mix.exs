cf2cefa7c21ffa47b0817b98e44cf9188fef5225
axelrizo <axelg.rizos@gmail.com>
2026-01-31 11:11:34 -0500

    chore: add Kaffy dashboard dependency

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule MyDiet.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # Phoenix and related dependencies
       {:phoenix, "~> 1.8.1"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
@@ -65,7 +66,9 @@ defmodule MyDiet.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      # Admin dashboard
+      {:kaffy, "~> 0.11.0"}
     ]
   end
 
