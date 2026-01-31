eac0a66b8cd98f48fccb8f6f6d2c704e52fb892a
axelrizo <axelg.rizos@gmail.com>
2026-01-31 12:24:39 -0500

    chore: update .recode.exs to version 0.8.0 and adjust task configurations

--- .recode.exs
+++ .recode.exs
@@ -1,5 +1,5 @@
 [
-  version: "0.7.3",
+  version: "0.8.0",
   # Can also be set/reset with `--autocorrect`/`--no-autocorrect`.
   autocorrect: true,
   # With "--dry" no changes will be written to the files.
@@ -10,6 +10,12 @@
   color: true,
   # Can also be set/reset with `--verbose`/`--no-verbose`.
   verbose: false,
+  # Can be overwritten with `--silent`/`--no-silent`.
+  # When enabled, suppresses all non-essential output during execution.
+  silent: false,
+  # Inputs can be a path, glob expression or list of paths and glob expressions.
+  # With the atom :formatter the inputs from .formatter.exs are
+  # used. also allowed in the list mentioned above.
   # Can be overwritten by calling `mix recode "lib/**/*.ex"`.
   inputs: [
     "{mix,.formatter}.exs",
@@ -18,6 +24,8 @@
     "lib/my_diet/**/*.ex"
   ],
   formatters: [Recode.CLIFormatter],
+  # Can also be set/reset with `--manifest`/`--no-manifest`.
+  manifest: true,
   tasks: [
     # Tasks could be added by a tuple of the tasks module name and an options
     # keyword list. A task can be deactivated by `active: false`. The execution of
@@ -29,14 +37,15 @@
     {Recode.Task.FilterCount, [active: false]},
     {Recode.Task.IOInspect, [autocorrect: false]},
     {Recode.Task.LocalsWithoutParens, [active: false]},
-    {Recode.Task.Moduledoc, [active: false]},
+    {Recode.Task.Moduledoc, [exclude: ["test/**/*.{ex,exs}", "mix.exs"], active: false]},
     {Recode.Task.Nesting, [active: false]},
     {Recode.Task.PipeFunOne, [active: false]},
     {Recode.Task.SinglePipe, []},
-    {Recode.Task.Specs, [active: false]},
-    {Recode.Task.TagFIXME, [active: false]},
-    {Recode.Task.TagTODO, [active: false]},
-    {Recode.Task.TestFileExt, [active: false]},
+    {Recode.Task.Specs,
+     [exclude: ["test/**/*.{ex,exs}", "mix.exs"], config: [only: :visible], active: false]},
+    {Recode.Task.TagFIXME, [exit_code: 2, active: false]},
+    {Recode.Task.TagTODO, [exit_code: 4, active: false]},
+    {Recode.Task.TestFile, []},
     {Recode.Task.UnnecessaryIfUnless, [active: false]},
     {Recode.Task.UnusedVariable, [active: false]}
   ]
