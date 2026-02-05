3303f460c2d0bb1f5aaf43270b8e4a30e7d73c03
axelrizo <axelg.rizos@gmail.com>
2026-02-05 18:41:00 -0500

    feat(deps): update absinthe to version 1.9 and add dataloader dependency

--- mix.exs
+++ mix.exs
@@ -130,8 +130,9 @@ defmodule MyDiet.MixProject do
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       # Graphql
-      {:absinthe, "~> 1.7"},
+      {:absinthe, "~> 1.9"},
       {:absinthe_plug, "~> 1.5"},
+      {:dataloader, "~> 1.0.0"},
       # Admin dashboard
       {:kaffy, "~> 0.11.0"},
       # Docs
