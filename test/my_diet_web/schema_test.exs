9ee59b07b6933d740a3875232fceb8c2f4742b90
axelrizo <axelg.rizos@gmail.com>
2026-02-05 23:23:34 -0500

    refactor(schema): update food queries for clarity and consistency

--- test/my_diet_web/schema_test.exs
+++ test/my_diet_web/schema_test.exs
@@ -1,7 +1,25 @@
 defmodule MyDietWeb.SchemaTest do
   use MyDietWeb.ConnCase, async: true
 
-  describe "query: allFoods" do
+  describe "query: foods" do
+    test "returns food list", %{conn: conn} do
+      food = insert(:food)
+
+      query = """
+      {
+        foods {
+          id
+          name
+        }
+      }
+      """
+
+      assert %{"foods" => [returned_food]} = query_graphql(conn, query)
+
+      assert food.id == String.to_integer(returned_food["id"])
+      assert food.name == returned_food["name"]
+    end
+
     @query """
     {
       foods {
@@ -26,19 +44,9 @@ defmodule MyDietWeb.SchemaTest do
     """
 
     test "returns a list of all foods", %{conn: conn} do
-      food_category = insert(:food_category)
-
-      insert(:food, %{
-        name: "Apple",
-        food_category: food_category,
-        food_measures: build_list(2, :food_measure)
-      })
-
-      insert(:food, %{
-        name: "Banana",
-        food_category: food_category,
-        food_measures: build_list(2, :food_measure)
-      })
+      :food_category
+      |> insert()
+      |> with_food()
 
       conn = post(conn, "/api", %{"query" => @query})
 
@@ -78,7 +86,7 @@ defmodule MyDietWeb.SchemaTest do
   describe "query: allMeals" do
     @query """
     {
-      allMeals {
+      meals {
         name
         mealIngredients {
           id
@@ -103,5 +111,12 @@ defmodule MyDietWeb.SchemaTest do
 
       _response = json_response(conn, 200)
     end
+  end
+
+  defp query_graphql(conn, query) do
+    conn
+    |> post("/api", %{"query" => query})
+    |> json_response(200)
+    |> Map.fetch!("data")
   end
 end
